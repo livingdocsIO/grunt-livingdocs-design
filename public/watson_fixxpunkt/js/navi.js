4f4f6c4cf3efb64f0403553d230a2f2812631ee1
Sven <sven.ruef@fixxpunkt.ch>
2013-07-05 14:10:33 +0200

    added small new version of talkback to lol-story

--- public/watson_fixxpunkt/js/navi.js
+++ public/watson_fixxpunkt/js/navi.js
@@ -86,6 +86,40 @@ document.addEventListener('touchmove', function(){$(".currentblock").hide();}, f
 
 
 
+var social = (function(){
+	return {
+		toggle_social:function() {
+			$(".socialblock .expandable").toggleClass("expanded");
+			if ($(".socialblock .expandable").hasClass("expanded")) social.show_social();
+			else social.hide_social();
+		},
+		
+		show_social:function() {
+			var social_width = $(".socialblock .expandable").width();
+			if ($(".wrapper").width()<=600) social_width = $(".wrapper").width();
+			$(".socialblock .expandable").css("left", -social_width+"px").show().animate({left: 0}, 500);
+		},
+		
+		hide_social:function() {
+			$(".socialblock .expandable").removeClass("expanded");
+			var social_width = $(".socialblock .expandable").width();
+			if ($(".wrapper").width()<=600) social_width = $(".wrapper").width();
+			$(".socialblock .expandable").animate({left: -social_width}, 500, function(){$(this).hide()});
+		},
+		
+		init:function() {
+			/* toggling the socialblock */
+			$(".socialblock .sociallink").click(function(){
+				social.toggle_social();
+			});
+		}
+	}
+})();
+
+$(document).ready( social.init );
+
+
+
 
 
 
