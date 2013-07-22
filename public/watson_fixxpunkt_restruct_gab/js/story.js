e7c3cde994361a79a6cb21bbd049456be9006009
Sven <sven.ruef@fixxpunkt.ch>
2013-07-22 17:09:14 +0200

    added js for adjusting cinemascope-inserts to window size

--- public/watson_fixxpunkt_restruct_gab/js/story.js
+++ public/watson_fixxpunkt_restruct_gab/js/story.js
@@ -1,10 +1,25 @@
 
 
 
-$(document).ready(function() {
-
-
-
+var inserts = (function(){
+	return {
+		adjust_cinemascope_inserts:function() {
+			if ( $(".insert.cinemascope").length>0 ) {
+				var window_width = $(window).width();
+				if (window_width>1000) {
+					var new_left = $(".wrapper").offset().left + parseInt( $(".story").css("marginLeft") );
+					$(".insert.cinemascope").width(window_width);
+					$(".insert.cinemascope").css("left", -new_left);
+				}
+				else {
+					$(".insert.cinemascope").css("width", "inherit");
+					$(".insert.cinemascope").css("left", "inherit");
+				}
+			}
+		}
+	}
+})();
 	
-	
-});
+
+$(window).resize(inserts.adjust_cinemascope_inserts);
+$(document).ready(inserts.adjust_cinemascope_inserts);
