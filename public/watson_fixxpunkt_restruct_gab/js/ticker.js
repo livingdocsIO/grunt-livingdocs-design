f1b53f3aa329a7b44ab1ab9e9ecfe2d3275c9e42
Sven <sven.ruef@fixxpunkt.ch>
2013-08-05 17:22:47 +0200

    ticker changes

--- public/watson_fixxpunkt_restruct_gab/js/ticker.js
+++ public/watson_fixxpunkt_restruct_gab/js/ticker.js
@@ -9,9 +9,12 @@
 $(document).ready(function(){
 	
 	$(".collapsa").click(function(){
-		$(this).siblings(".colltent").slideToggle();
+		$(this).toggleClass("expanded").siblings(".colltent").slideToggle();
 		$(this).blur();
 	});
+	$(".colltent:visible").siblings(".collapsa").addClass("expanded");
+	
+
 	
 	$(".widget.ticker .main_result .collapse, .widget.ticker .main_result .expand").click(function(){
 		$(".widget.ticker .main_result").toggleClass("expanded");
