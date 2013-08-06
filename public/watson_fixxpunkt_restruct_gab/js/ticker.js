1d46379a81fee42d1e3d05729262a717c0628ec2
Sven <sven.ruef@fixxpunkt.ch>
2013-08-06 10:38:19 +0200

    further ticker html

--- public/watson_fixxpunkt_restruct_gab/js/ticker.js
+++ public/watson_fixxpunkt_restruct_gab/js/ticker.js
@@ -8,17 +8,18 @@
 	
 $(document).ready(function(){
 	
-	$(".collapsa").click(function(){
-		$(this).toggleClass("expanded").siblings(".colltent").slideToggle();
-		$(this).blur();
+	$(".widget.ticker .collapsa:not(.inactive)").click(function(){
+		$(this).toggleClass("expanded").siblings(".colltent").slideToggle().blur();
 	});
-	$(".colltent:visible").siblings(".collapsa").addClass("expanded");
-	
-
+	$(".widget.ticker .colltent:visible").siblings(".collapsa:not(.inactive)").addClass("expanded");
 	
 	$(".widget.ticker .main_result .collapse, .widget.ticker .main_result .expand").click(function(){
 		$(".widget.ticker .main_result").toggleClass("expanded");
 		$(this).blur();
+	});
+	
+	$(".widget.ticker .tools a.order").click(function(){
+		$(this).toggleClass("up").blur();
 	});
 	
 });
