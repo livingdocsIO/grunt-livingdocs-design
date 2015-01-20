25bcac376c689c597ab2a12c1155d5201c300bee
Marc Bachmann <marc.brookman@gmail.com>
2015-01-20 18:00:48 +0100

    Move compiler to separate module, remove coffee-script dependency.

---
+++ tasks/index.js
@@ -0,0 +1,30 @@
+var Design = require('livingdocs-design-manager');
+
+module.exports = function(grunt) {
+  // grunt task to compile a specific design
+  grunt.registerMultiTask('lddesign', 'Compile a single design', function() {
+    var design = this.files[0];
+    var done = this.async();
+    var options = this.options();
+    options.src = design.src[0];
+    options.dest = design.dest;
+
+    var error = null;
+    // Initialize design
+    Design.compile(options)
+    .on('debug', function(debug) { grunt.log.debug(debug); })
+    .on('warn', function(warning) { grunt.fail.warn(warning); })
+    .on('error', function(err) { error = err; })
+    .on('end', function() {
+      if (error) {
+        grunt.log.error(error);
+        done(false);
+      }
+      else {
+        grunt.log.write("Design compiled...");
+        grunt.log.ok();
+        done();
+      }
+    });
+  });
+}
