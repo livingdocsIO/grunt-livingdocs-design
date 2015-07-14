16c21bd990e37cc1ff39dab89cf86a494ba35faf
Marc Bachmann <marc.brookman@gmail.com>
2015-07-14 17:31:53 +0200

    Remove unnesessary files We manually edit the package.json version when doing a release. Tests can be run with `npm test`

--- Changelog.md
+++
@@ -1,9 +0,0 @@
-
-# v0.2.0
-
-Extract compiler to separate commonjs module.
-
-
-# v0.1.0
-
-Initial Release
--- tasks/index.js
+++ tasks/index.js
@@ -1,30 +1,32 @@
-var Design = require('livingdocs-manager');
+var Design = require('livingdocs-manager')
 
-module.exports = function(grunt) {
+module.exports = function initializeLivingdocsDesignTasks (grunt) {
   // grunt task to compile a specific design
-  grunt.registerMultiTask('lddesign', 'Compile a single design', function() {
-    var design = this.files[0];
-    var done = this.async();
-    var options = this.options();
-    options.src = design.src[0];
-    options.dest = design.dest;
+  grunt.registerMultiTask('lddesign', 'Compile a single design', processLivingdocsDesign)
 
-    var error = null;
+  function processLivingdocsDesign () {
+    var design = this.files[0]
+    var done = this.async()
+    var options = this.options()
+    options.src = design.src[0]
+    options.dest = design.dest
+
+    var error = null
     // Initialize design
     Design.build(options)
-    .on('debug', function(debug) { grunt.log.debug(debug); })
-    .on('warn', function(warning) { grunt.fail.warn(warning); })
-    .on('error', function(err) { error = err; })
-    .on('end', function() {
+    .on('debug', function onDebug (debug) { grunt.log.debug(debug) })
+    .on('warn', function onWarning (warning) { grunt.fail.warn(warning) })
+    .on('error', function onError (err) { error = err })
+    .on('end', function onEnd () {
       if (error) {
-        grunt.log.error(error);
-        done(false);
+        grunt.log.error(error)
+        done(false)
+      } else {
+        grunt.log.write('Design compiled...')
+        grunt.log.ok()
+        done()
       }
-      else {
-        grunt.log.write("Design compiled...");
-        grunt.log.ok();
-        done();
-      }
-    });
-  });
+    })
+  }
+
 }
