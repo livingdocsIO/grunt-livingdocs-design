a1e8fe654c001107756cac349db9b99254eb058b
Marc Bachmann <marc.brookman@gmail.com>
2021-10-19 14:57:23 +0200

    chore: Add .eslintrc.json

--- tasks/index.js
+++ tasks/index.js
@@ -1,32 +1,32 @@
-var Design = require('livingdocs-manager')
+const Design = require('livingdocs-manager')
 
 module.exports = function initializeLivingdocsDesignTasks (grunt) {
   // grunt task to compile a specific design
   grunt.registerMultiTask('lddesign', 'Compile a single design', processLivingdocsDesign)
 
   function processLivingdocsDesign () {
-    var design = this.files[0]
-    var done = this.async()
-    var options = this.options()
+    const design = this.files[0]
+    const done = this.async()
+    const options = this.options()
     options.src = design.src[0]
     options.dest = design.dest
 
-    var error = null
+    let error = null
     // Initialize design
     Design.build(options)
-    .on('debug', function onDebug (debug) { grunt.log.debug(debug) })
-    .on('warn', function onWarning (warning) { grunt.fail.warn(warning) })
-    .on('error', function onError (err) { error = err })
-    .on('end', function onEnd () {
-      if (error) {
-        grunt.log.error(error)
-        done(false)
-      } else {
-        grunt.log.write('Design compiled...')
-        grunt.log.ok()
-        done()
-      }
-    })
+      .on('debug', function onDebug (debug) { grunt.log.debug(debug) })
+      .on('warn', function onWarning (warning) { grunt.fail.warn(warning) })
+      .on('error', function onError (err) { error = err })
+      .on('end', function onEnd () {
+        if (error) {
+          grunt.log.error(error)
+          return done(false)
+        } else {
+          grunt.log.write('Design compiled...')
+          grunt.log.ok()
+          return done()
+        }
+      })
   }
 
 }
